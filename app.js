5550150cddeab30b4083227ff455b687d22f9b44
Adam McMahon <obelesk411@users.noreply.github.com>
2016-11-22 16:54:53 -0700

    chore: setup db connection

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var config = require('./config/index.js');
-var app = express();
+var mongoose = require('mongoose');
+var config = require('./config');
 
-console.log(config.getDbConnectionString());
+var app = express();
 
 var port = process.env.PORT || 3000;
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+mongoose.connect(config.getDbConnectionString());
 
 app.listen(port);
